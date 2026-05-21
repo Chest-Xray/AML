796e518c9a8a9d15b2af0e1047247e70b2f47fb2
Ids <idshummel@gmail.com>
2026-05-21 17:12:59 +0200

    add wandb

--- habrok/train_one.sh
+++ habrok/train_one.sh
@@ -7,4 +7,5 @@
 module purge
 module load CUDA-Python/12.6.2.post1-gfbf-2024a-CUDA-12.6.0
 source venv/bin/activate
+export WANDB_API_KEY=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
 srun --nodes=1 --gpus-per-node=1 --time=20:00 --mem=32G --cpus-per-gpu=8 python3 -m main $1
