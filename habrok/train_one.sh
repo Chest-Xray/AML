8ac826fff65859c8dbe8889d71a511be780a7e10
Ids <idshummel@gmail.com>
2026-05-21 17:02:15 +0200

    add `module purge` to habrok script

--- habrok/train_one.sh
+++ habrok/train_one.sh
@@ -4,7 +4,7 @@
 #SBATCH --gpus-per-node=1
 #SBATCH --mem=32G
 #SBATCH --cpus-per-gpu=8
-
+module purge
 module load CUDA-Python/12.6.2.post1-gfbf-2024a-CUDA-12.6.0
 source venv/bin/activate
 srun --nodes=1 --gpus-per-node=1 --time=20:00 --mem=32G --cpus-per-gpu=8 python3 -m main $1
