c896b1613ca4fb5379b435942624e9488e3a067b
Ids <idshummel@gmail.com>
2026-05-21 17:01:29 +0200

    fix habrok script

--- habrok/train_one.sh
+++ habrok/train_one.sh
@@ -3,8 +3,8 @@
 #SBATCH --partition=gpu
 #SBATCH --gpus-per-node=1
 #SBATCH --mem=32G
-#SBATCH --cpus=4
+#SBATCH --cpus-per-gpu=8
 
 module load CUDA-Python/12.6.2.post1-gfbf-2024a-CUDA-12.6.0
 source venv/bin/activate
-srun --nodes=1 --gpus-per-node=1 --time=20:00 --mem=32G --cpus=4 python3 -m main $1
+srun --nodes=1 --gpus-per-node=1 --time=20:00 --mem=32G --cpus-per-gpu=8 python3 -m main $1
