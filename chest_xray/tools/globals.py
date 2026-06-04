58edef29ae5138de6fcce7f0cc4e22d380e6030c
Ids <idshummel@gmail.com>
2026-06-04 14:00:42 +0200

    increase max number of epochs to 25

--- chest_xray/tools/globals.py
+++ chest_xray/tools/globals.py
@@ -2,5 +2,5 @@ BATCH_SIZE: int = 4    # play around with this on Habrok
 SEED: int = 42
 NUM_WORKERS: int = 4    # play around with this on Habrok
 K_FOLDS: int = 4
-NUM_EPOCHS: int = 15
+NUM_EPOCHS: int = 25
 MODEL_PATH: Path = Path(__file__).parent.parent / "data" / "models"
