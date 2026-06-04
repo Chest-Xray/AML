a4111d0cfa9da4cc39276b0c159e1ae7ab540aa4
Ids <idshummel@gmail.com>
2026-06-04 14:15:31 +0200

    fix habrok scripts

--- habrok/train_all.sh
+++ habrok/train_all.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-for item in baseline pretrained scratch; do
-    ./train_one.sh $item
+for model in baseline densenet161 densenet201; do
+    if [ "$item" == "baseline" ]; then
+        for training in pretrained scratch; do
+            sbatch ./train_one.sh $model $training
+        done
+    else
+        sbatch ./train_one.sh $model
+    fi
 done
