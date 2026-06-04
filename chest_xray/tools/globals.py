5fa159b3eacc866565e3da2ebe2586f43b0cd1c7
Ids <idshummel@gmail.com>
2026-06-04 13:29:23 +0200

    fix model location to be relative to directory location

--- chest_xray/tools/globals.py
+++ chest_xray/tools/globals.py
@@ -3,4 +3,4 @@ SEED: int = 42
 NUM_WORKERS: int = 4    # play around with this on Habrok
 K_FOLDS: int = 4
 NUM_EPOCHS: int = 15
-MODEL_PATH: str = "/home/ids/Documents/uni/AML/chest_xray/data/models/"
+MODEL_PATH: Path = Path(__file__).parent.parent / "data" / "models"
